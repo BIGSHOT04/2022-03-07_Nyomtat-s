1c29c031051f0df9d9251e3cd0aa80fc648865cf
Árki <95213390+BIGSHOT04@users.noreply.github.com>
2022-03-11 09:11:24 +0100

    4

--- nyomtatas.js
+++ nyomtatas.js
@@ -1,7 +1,7 @@
 function kalkulal(){
     //Űrlapadatok
-    const szelesseg=document.getElementById('szelesseg');
-    const magassag=document.getElementById('magassag');
+    const szelesseg=297;
+    const magassag=420;
     const papir=document.getElementById('papirtipus').value;
     //Számítások
     let terulet=Math.round((szelesseg*magassag)/10000);    
